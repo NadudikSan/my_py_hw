e13a5ae21085d56d6ede6ec799c70adcb5354a9a
NadudikSan <sanya_nadudik2@mail.ru>
2022-03-03 11:05:59 +0300

    Add homework5.4.py

--- HomeWork5.py
+++ HomeWork5.py
@@ -1,35 +1,35 @@
-# #HomeWork5.1
-# #factorial function
-# def fact(n):
-#     sum = 1
-#     if n < 0:
-#         return None
-#     elif n <= 1:
-#         return 1
-#     else:
-#         for i in range(2, n+1):
-#             sum *= i
-#     return sum
-# #Output of the factorial sum
-# for i in range(-1,9):
-#     print(fact(i))
+#HomeWork5.1
+#factorial function
+def fact(n):
+    sum = 1
+    if n < 0:
+        return None
+    elif n <= 1:
+        return 1
+    else:
+        for i in range(2, n+1):
+            sum *= i
+    return sum
+#Output of the factorial sum
+for i in range(-1,9):
+    print(fact(i))
 
-# #HomeWork5.2
-# # #Fibonacci function
-# def fib(val):
-#     f = [0,1,1]
-#     if val < 1:
-#         return None
-#     elif val < 3:
-#         return 1
-#     else: 
-#         for i in range(3, val):
-#             f.append(f[i-2]+f[i-1])
-#         ln = len(f)
-#         return (f[ln-1] + f[ln-2])
+#HomeWork5.2
+# #Fibonacci function
+def fib(val):
+    f = [0,1,1]
+    if val < 1:
+        return None
+    elif val < 3:
+        return 1
+    else: 
+        for i in range(3, val):
+            f.append(f[i-2]+f[i-1])
+        ln = len(f)
+        return (f[ln-1] + f[ln-2])
 
-# for i in range(-1,25):
-#     print(fib(i))
+for i in range(-1,25):
+    print(fib(i))
 
 #HomeWork5.3
 import random
@@ -38,19 +38,20 @@ pcRes = 0
 userRes = 0
 def userMove():
     while True:
-        userVal = int(input("Your move:"))
+        userVal = int(input("Your move: "))
         if board.count(userVal) == 0:
             print("The cell is occupied")
         else:
-            for i in range(0,9):
+            for i in range(9):
                     if board[i] == userVal:
                         board[i] = "O"
                         return
 def pcMove():
     while True:
-        pcVal = random.randrange(1,9)
+        pcVal = random.randrange(1,10)
+        print("PC move: ",pcVal)
         if board.count(pcVal) != 0:
-            for i in range(0,9):
+            for i in range(9):
                 if board[i] == pcVal:
                     board[i] = "X"
                     return
@@ -58,19 +59,25 @@ def checkResult():
     global pcRes
     global userRes
     #Horizontal check
-    if (board[0] == board[1] == board[2] == "X") or (board[3] == board[4] == board[5] == "X") or (board[6] == board[7] == board[8] == "X"):
-        pcRes += 1
-    elif (board[0] == board[1] == board[2] == "O") or (board[3] == board[4] == board[5] == "O") or (board[6] == board[7] == board[8] == "O"):
-        userRes += 1
+    for i in range(0,9,3):
+        if board[i] == board[i+1] == board[i+2] == "X":
+            pcRes += 1
+        elif board[i] == board[i+1] == board[i+2] == "O":
+            userRes += 1
     #Vertical check
-    if (board[0] == board[3] == board[6] == "X") or (board[1] == board[4] == board[7] == "X") or (board[2] == board[5] == board[8] == "X"):
-        pcRes += 1
-    elif (board[0] == board[3] == board[6] == "O") or (board[1] == board[4] == board[7] == "O") or (board[2] == board[5] == board[8] == "O"):
-        userRes += 1
+    for i in range(3):
+        if board[i] == board[i+3] == board[i+6] == "X":
+            pcRes += 1
+        elif board[i] == board[i+3] == board[i+6] == "O":
+            userRes += 1
     #Diagonal result
-    if (board[2] == board[4] == board[6] == "X") or (board[0] == board[4] == board[8] == "X") :
+    if (board[2] == board[4] == board[6] == "X"):
         pcRes += 1
-    elif (board[2] == board[4] == board[6] == "O") or (board[0] == board[4] == board[8] == "O"):
+    elif (board[0] == board[4] == board[8] == "X"):
+        pcRes += 1
+    if (board[2] == board[4] == board[6] == "O"):
+        userRes += 1
+    elif (board[0] == board[4] == board[8] == "O"):
         userRes += 1
 def displayBoard(board):
     print("+-------" * 3,"+", sep="")
@@ -84,23 +91,31 @@ def displayBoard(board):
         print("|       " * 3, "|", sep="")
         print("+-------" * 3, "+", sep="")
         
-displayBoard(board)
 #The first move
 board[4] = "X"
 celling = len(board)
-
-while celling > 1:
+displayBoard(board)
+while celling >= 2:
     userMove()
     displayBoard(board)
     pcMove()
     displayBoard(board)
-    checkResult()
     celling -= 2
-displayBoard(board)
-print("Result PC = ",pcRes,"User = ",userRes, sep="|")
+checkResult()
+print("Result PC = ",pcRes,"User = ",userRes,"|", sep="|")
 if userRes > pcRes:
     print("User Win")
-elif pcRes > userRes:
+elif userRes < pcRes:
     print("PC Win")
 else:
     print("The result is a draw")
+
+#HomeWork5.4
+import time
+messageUser = input("Enter message:")
+with open('note.txt','w') as file:
+    while messageUser != "end":
+        named_tuple = time.localtime() # receive struct_time
+        time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+        file.write(time_string +" "+ messageUser+"\n" )
+        messageUser = input("Enter message:")
