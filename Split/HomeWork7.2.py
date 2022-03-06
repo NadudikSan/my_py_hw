4bfd79274477eba3dc635514c7c8d9e8b8f8543c
NadudikSan <sanya_nadudik2@mail.ru>
2022-03-06 17:43:36 +0300

    My HW to L7

---
+++ Split/HomeWork7.2.py
@@ -0,0 +1,13 @@
+
+def mysplit(str):
+    myList = []
+    newStr = ""
+    for i in str:
+        if i.isspace() != True:
+            newStr += i
+        else:
+            myList.append(newStr)
+            newStr = ""
+    myList.append(newStr)    
+    return myList
+print(mysplit("My split to be"))
